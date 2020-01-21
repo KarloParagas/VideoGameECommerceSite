f1f438884b5bcc15fc3f61e85db544b60afd19e0
KarloParagas <karloparagas@gmail.com>
2020-01-20 17:40:50 -0800

    Created database context class with VideoGame model

---
+++ eCommerce/Data/GameContext.cs
@@ -0,0 +1,24 @@
+﻿using eCommerce.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Data
+{
+    /// <summary>
+    /// The database context class for the video game store
+    /// </summary>
+    public class GameContext : DbContext //":" is Java's equivalent of "inherits"
+    {
+        public GameContext(DbContextOptions<GameContext> options) : base(options) //base(parameter) is similar to Java's "super" keyword 
+        {
+            //Don't need anything in this constructor, because we're passing in our DbContextOptions object to the base class, and the base class
+            //handles all the functionality
+        }
+
+        //Add a DbSet<T> for each entity you want to keep track of in the database
+        public DbSet<VideoGame> VideoGames { get; set; }
+    }
+}
